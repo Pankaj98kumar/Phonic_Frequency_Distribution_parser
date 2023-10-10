7aed46420da215cab34124bd7ede1ba127ebfb1e
Pankaj <pankaj@code.com>
2023-10-10 20:48:18 +0530

    Final Commit

--- Project/Home/views.py
+++ Project/Home/views.py
@@ -10,20 +10,23 @@ from pydub import AudioSegment
 def home(request):
     plt.close("all")
     if(request.method=='POST'):
+        # Reading input file and taking fourier transform of file
         file=request.FILES['data']
         rate, data = wav.read(file)
         fft_out = fft(data)
 
+        # Plotting spectrum of the input file
         plt.plot(data, np.abs(fft_out))
-
-       
         fig=plt.gcf()
         buf=io.BytesIO()
         fig.savefig(buf,format='png')
         buf.seek(0)
+
+        # Converting file in binary file
         string =base64.b64encode(buf.read())
         uri=urllib.parse.quote(string)
-        print(uri)
+
+        # Rendering Template Files
         return render(request,"Home/form.html",{'data':uri})
         
     else:
